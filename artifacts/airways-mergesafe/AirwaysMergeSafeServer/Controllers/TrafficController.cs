cc7ef637e9e3081e966c859232edb213f6052574
daluvalanokia <daluvalanokia@gmail.com>
2026-07-27 16:11:11 -0400

    fix: add TraceLogger Enter/Exit/Error to TrafficController

--- artifacts/airways-mergesafe/AirwaysMergeSafeServer/Controllers/TrafficController.cs
+++ artifacts/airways-mergesafe/AirwaysMergeSafeServer/Controllers/TrafficController.cs
@@ -4,6 +4,7 @@ using AirwaysMergeSafeServer.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OutputCaching;
 using Microsoft.EntityFrameworkCore;
+using AirwaysMergeSafeServer.Infrastructure;
 
 namespace AirwaysMergeSafeServer.Controllers;
 
@@ -17,6 +18,7 @@ public class TrafficController : Controller
 
     public async Task<IActionResult> Index(string? highwayId)
     {
+        TraceLogger.Enter("Traffic", nameof(Index));
         var highways = await _db.Highways.AsNoTracking().Where(h => h.IsActive).OrderBy(h => h.Name).ToListAsync();
         highwayId ??= HttpContext.Session.GetString("HighwayId") ?? highways.FirstOrDefault()?.HighwayId;
         if (highwayId != null) HttpContext.Session.SetString("HighwayId", highwayId);
@@ -24,6 +26,7 @@ public class TrafficController : Controller
         var zones   = await _db.MergeZones.AsNoTracking().Where(z => z.HighwayId == highwayId).ToListAsync();
         var sensors = await _db.SensorDevices.AsNoTracking().Where(d => d.HighwayId == highwayId).ToListAsync();
 
+        TraceLogger.Exit("Traffic", nameof(Index));
         return View(new Traffic3DViewModel
         {
             Highways          = highways,
@@ -37,7 +40,9 @@ public class TrafficController : Controller
     [HttpGet, OutputCache(PolicyName = "ShortLive")]
     public async Task<IActionResult> GetSegments(string highwayId)
     {
+        TraceLogger.Enter("Traffic", nameof(GetSegments));
         var result = await _trafficSvc.GetSegmentsAsync(highwayId);
+        TraceLogger.Exit("Traffic", nameof(GetSegments));
         return Json(result);
     }
 }
